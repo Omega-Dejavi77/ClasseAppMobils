4283e1d144a092a045d7bdbb4e462f7b59a888c3
gillemp <guplem@gmail.com>
2020-04-29 11:09:53 +0200

    Save data so rotation works

--- RecyclerViewProject/app/src/main/java/com/example/recyclerviewproject/MainActivity.java
+++ RecyclerViewProject/app/src/main/java/com/example/recyclerviewproject/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.recyclerviewproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,7 +33,12 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         collecionView.setLayoutManager(layoutManager);
 
-        CreateDummyContent();
+        if (savedInstanceState == null) { //It is NOT a 'reboot' or 'reload'
+            CreateDummyContent();
+        } else { //It is a 'reboot' or 'reload', so restore data
+            dataSet = savedInstanceState.getParcelableArrayList("dataSet");
+            todoText.setText(savedInstanceState.getString("todoText"));
+        }
 
         todoAdapter = new TodoAdapter(dataSet);
         collecionView.setAdapter(todoAdapter);
@@ -55,4 +62,25 @@ public class MainActivity extends AppCompatActivity {
         dataSet.add(new Todo("Task 3"));
         dataSet.add(new Todo("Task 4"));
     }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelableArrayList("dataSet", dataSet);
+        outState.putString("todoText", todoText.getText().toString());
+
+        super.onSaveInstanceState(outState); // Do last
+    }
+
+    // ALTERNATIVE VERSION TO RESTORE DATA
+    /*@Override
+    public void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState); // Do first
+
+        // Be careful it must be created after storing the data in the set
+        todoAdapter = new TodoAdapter(dataSet);
+        collecionView.setAdapter(todoAdapter);
+
+        CreateAndSetTodoAdapter();
+    }*/
+
 }
--- RecyclerViewProject/app/src/main/java/com/example/recyclerviewproject/Todo.java
+++ RecyclerViewProject/app/src/main/java/com/example/recyclerviewproject/Todo.java
@@ -1,8 +1,11 @@
 package com.example.recyclerviewproject;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Random;
 
-public class Todo {
+public class Todo implements Parcelable {
     private int id;
     private String task;
 
@@ -10,6 +13,23 @@ public class Todo {
         this.task = task;
         this.id = new Random().nextInt(9999);
     }
+
+    protected Todo(Parcel in) {
+        super();
+        readFromParcel(in);
+    }
+
+    public static final Creator<Todo> CREATOR = new Creator<Todo>() {
+        @Override
+        public Todo createFromParcel(Parcel in) {
+            return new Todo(in);
+        }
+
+        @Override
+        public Todo[] newArray(int size) {
+            return new Todo[size];
+        }
+    };
 
     public int getId() {
         return id;
@@ -25,5 +45,22 @@ public class Todo {
 
     public void setTask(String task) {
         this.task = task;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    private void readFromParcel(Parcel in) {
+        // The 'readXXX' methods of a Parcel reads the elements by order, so the insertion order must be taken into account.
+        id = in.readInt(); // Reads the first int
+        task = in.readString(); // Reads the first string
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id); // Writes the first int
+        dest.writeString(task); // Writes the first string
     }
 }
