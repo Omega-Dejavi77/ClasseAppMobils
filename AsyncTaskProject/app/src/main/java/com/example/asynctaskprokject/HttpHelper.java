5886dfb29b43ca61b1ae9a37a0a0c5fb4e605b66
Javier de la Serna Martínez <37005341+Omega-Dejavi77@users.noreply.github.com>
2020-05-20 10:40:02 +0200

    async task created

---
+++ AsyncTaskProject/app/src/main/java/com/example/asynctaskprokject/HttpHelper.java
@@ -0,0 +1,11 @@
+package com.example.asynctaskprokject;
+
+import android.os.AsyncTask;
+
+public class HttpHelper extends AsyncTask<String, String, String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
