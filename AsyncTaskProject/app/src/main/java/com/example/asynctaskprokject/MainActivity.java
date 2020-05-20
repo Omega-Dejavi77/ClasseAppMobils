906dd1d77ba6cd3dbc2050b1a7934772254b16e6
Javier de la Serna Martínez <37005341+Omega-Dejavi77@users.noreply.github.com>
2020-05-20 12:04:43 +0200

    Json default read

--- AsyncTaskProject/app/src/main/java/com/example/asynctaskprokject/MainActivity.java
+++ AsyncTaskProject/app/src/main/java/com/example/asynctaskprokject/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.asynctaskprokject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements HttpHelper.HttpHelperResponseHandler {
 
@@ -24,6 +29,38 @@ public class MainActivity extends AppCompatActivity implements HttpHelper.HttpHe
 
     @Override
     public void setHttpHelperResponse(String result) {
-        jsonContent.setText(result);
+        //jsonContent.setText(result);
+        jsonContent.setText(getAllNames(result));
+        jsonContent.setText(getFirstUser(result));
+    }
+
+    private String getAllNames(String rawJson) {
+        try {
+            String result = "";
+            JSONArray users = new JSONArray(rawJson);
+            for (int i = 0; i < users.length(); i++) {
+                JSONObject currentUser = users.getJSONObject(i);
+                if (currentUser.has("name"))
+                    result += currentUser.getString("name") + "\n";
+            }
+            return result;
+        } catch (Exception e) {
+            Log.d("SwA", "Error parsing names" + e.getMessage());
+        }
+
+        return "";
+    }
+
+    private String getFirstUser(String rawJson) {
+        try {
+            String result = "";
+            JSONArray users = new JSONArray(rawJson);
+            return users.getJSONObject(0).toString();
+
+        } catch (Exception e) {
+            Log.d("SwA", "Error parsing names" + e.getMessage());
+        }
+
+        return "";
     }
 }
