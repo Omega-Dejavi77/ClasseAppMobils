54a93f8bfa3214df6b30873ee27cec34bfc388e8
Javier de la Serna Martínez <37005341+Omega-Dejavi77@users.noreply.github.com>
2020-05-20 11:19:04 +0200

    example finished

--- AsyncTaskProject/app/src/main/java/com/example/asynctaskprokject/HttpHelper.java
+++ AsyncTaskProject/app/src/main/java/com/example/asynctaskprokject/HttpHelper.java
@@ -1,11 +1,64 @@
 package com.example.asynctaskprokject;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HttpHelper extends AsyncTask<String, String, String> {
 
+    public HttpHelperResponseHandler delegate = null;
+
     @Override
-    protected String doInBackground(String... strings) {
-        return null;
+    protected String doInBackground(String... urls) {
+        String result = "";
+        for (String currentUrl : urls) {
+            try{
+                URL url = new URL(currentUrl);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                int responseCode = urlConnection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    try{
+                        InputStream is = urlConnection.getInputStream();
+                        result = readBody(is);
+
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                }
+
+            } catch (Exception e){
+                Log.d("SwA", "Error --> Load content from" + currentUrl + "=" + e.getMessage());
+            }
+        }
+        return result;
+    }
+
+    private String readBody(InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder str = new StringBuilder();
+
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            str.append(line);
+        }
+        is.close();
+        return str.toString();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if (delegate != null) {
+            delegate.setHttpHelperResponse(result);
+        }
+    }
+
+    public interface HttpHelperResponseHandler {
+        void setHttpHelperResponse(String result);
     }
 }
--- AsyncTaskProject/app/src/main/java/com/example/asynctaskprokject/MainActivity.java
+++ AsyncTaskProject/app/src/main/java/com/example/asynctaskprokject/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.asynctaskprokject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HttpHelper.HttpHelperResponseHandler {
+
+    private TextView jsonContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jsonContent = findViewById(R.id.json_content);
+
+        HttpHelper task = new HttpHelper();
+        task.delegate = this;
+        String url = "https://jsonplaceholder.typicode.com/users/";
+        task.execute(url);
+    }
+
+    @Override
+    public void setHttpHelperResponse(String result) {
+        jsonContent.setText(result);
     }
 }
