6e5b0aeecef009e034fcdfced79a18b4653e30ce
Javier de la Serna Martínez <37005341+Omega-Dejavi77@users.noreply.github.com>
2020-05-20 12:45:40 +0200

    Simple request using Volley library

--- VolleyExample/app/src/main/java/com/example/volleyexample/MainActivity.java
+++ VolleyExample/app/src/main/java/com/example/volleyexample/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.volleyexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView jsonResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jsonResult = findViewById(R.id.json_result);
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "https://jsonplaceholder.typicode.com/users/";
+        fetchData(url, queue);
+    }
+
+    private void fetchData(String url, RequestQueue queue) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    jsonResult.setText(response);
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("SwA", "Error in request");
+            }
+        });
+
+        queue.add(stringRequest);
     }
 }
